9da5da554a39ea7d238b560980c67989e136b3bf
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-05-23 14:40:01 +0200

    fixed aur

--- src/commands/aur.ts
+++ src/commands/aur.ts
@@ -22,15 +22,18 @@ export default class aur extends Command {
 			],
 		});
 	}
-	async exec(message: naticoMessage, { args }: { args: string }) {
-		const pkg = await data(args);
-		if (!pkg || !pkg?.data?.results[0])
-			return message.reply(
-				'<:no:838017092216946748> Please provide a valid AUR package'
-			);
-
-		const result = pkg.data.results[0];
-		const embed = this.handler
+	async fetch(q: string) {
+		return await axiod(`https://aur.archlinux.org/rpc/`, {
+			method: 'GET',
+			params: {
+				v: 5,
+				type: 'search',
+				arg: q,
+			},
+		});
+	}
+	makeEmbed(result: any) {
+		return this.handler
 			.embed()
 			.setColor('#0080ff')
 			.addField('❯ Version', result.Version || 'no version')
@@ -42,8 +45,17 @@ export default class aur extends Command {
 			.setDescription(result.Description || 'No description provided')
 			.setTitle(
 				`<:arch:844981756246622209> ${result.Name}`,
-				result.URL || 'https://duckduckgo.com'
+				result.URL ?? 'https://duckduckgo.com'
 			);
+	}
+	async exec(message: naticoMessage, { args }: { args: string }) {
+		const pkg = await this.fetch(args);
+		if (!pkg || !pkg?.data?.results[0])
+			return message.reply(
+				'<:no:838017092216946748> Please provide a valid AUR package'
+			);
+
+		const embed = this.makeEmbed(pkg.data.results[0]);
 
 		message.channel?.send({
 			embed,
@@ -52,44 +64,16 @@ export default class aur extends Command {
 
 	async execSlash(
 		interaction: naticoInteraction,
-		{ aur }: { aur: { value: string } }
+		{ arch }: { arch: { value: string } }
 	) {
-		const pkg = await data(aur.value);
+		const pkg = await this.fetch(arch.value);
 
 		if (!pkg || !pkg.data.results[0])
 			return interaction.reply({
 				content: '<:no:838017092216946748> Please provide a valid AUR package',
 			});
 
-		const result = pkg.data.results[0];
-
-		const embed = this.handler
-			.embed()
-			.setColor('#0080ff')
-			.addField('❯ Version', result.Version || 'no version')
-			.addField('❯ Maintainer', result.Maintainer || 'no Maintainer')
-			.addField(
-				'❯ Popularity',
-				`${result.Popularity}, no clue but its on the api` || 'no Maintainer'
-			)
-			.setDescription(result.Description || 'No description provided')
-			.setTitle(`<:arch:844981756246622209> ${result.Name}`, `${result.URL}`);
-
-		interaction.edit({ content: 'aur', embeds: [embed] });
-	}
-}
-function data(q: string) {
-	try {
-		return axiod(`https://aur.archlinux.org/rpc/`, {
-			method: 'GET',
-			params: {
-				v: 5,
-				type: 'search',
-				arg: q,
-			},
-		});
-	} catch (e) {
-		e;
-		return false;
+		const embed = this.makeEmbed(pkg.data.results[0]);
+		interaction.reply({ content: '', embeds: [embed] });
 	}
 }
